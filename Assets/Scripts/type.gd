14d72eaee75790a69772fc688ba77587907f6824
Ryan Wang <s4884364@student.uq.edu.au>
2026-05-08 22:28:11 +1000

    wip: added label thing (still need to update finger values)

--- Assets/Scripts/type.gd
+++ Assets/Scripts/type.gd
@@ -2,9 +2,10 @@ extends Node2D
 
 var active_enemy = null
 var current_letter_index: int = -1 # undefined
-
+var fingers_remaining: int = 10 
+var prev_keycode: int = -1 # undefined for now
 @onready var enemy_container = $EnemyContainer
-
+@onready var fingers_label = $"CanvasLayer/VBoxContainer/BottomRow/fingers-value"
 var current_mistakes: String = ""
 
 #func _ready() -> void:
@@ -29,6 +30,9 @@ func _check_win(prompt: String) -> void:
 	if current_letter_index == prompt.length() and current_mistakes.length() == 0:
 		print("done")
 
+func _game_over() -> void:
+	return
+
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventKey and event.is_pressed() and not event.is_echo():
 		var typed_event = event as InputEventKey
@@ -37,11 +41,18 @@ func _unhandled_input(event: InputEvent) -> void:
 			find_new_active_enemy(key_typed)
 			return
 		if event.keycode == KEY_BACKSPACE:
+			if prev_keycode != event.keycode:
+				## didnt hold down backspace, first press, so decrement the fingers
+				fingers_remaining -= 1
+				if fingers_remaining == 0:
+					# game over
+					_game_over()
 			if current_mistakes.length() > 0:
 				current_mistakes = current_mistakes.erase(current_mistakes.length() - 1)
 			elif current_letter_index > 0:
 				current_letter_index -= 1
 			active_enemy.set_next_character(current_letter_index, current_mistakes)
+			prev_keycode = event.keycode
 			return
 		var prompt = active_enemy.get_prompt()
 		var next_character = prompt.substr(current_letter_index, 1)
@@ -56,6 +67,7 @@ func _unhandled_input(event: InputEvent) -> void:
 				print("incorrectly typed %s instead of %s" % [key_typed, next_character])
 		active_enemy.set_next_character(current_letter_index, current_mistakes)
 		_check_win(prompt)
+		prev_keycode = event.keycode
 
 #func start_game():
 	##game_over_screen.hide()
