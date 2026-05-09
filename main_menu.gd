4396b4e3e845310482885b642022f1c428694bac
Augustus Chong <225411179+Augustus-Chong@users.noreply.github.com>
2026-05-09 15:39:20 +1000

    Basic Main Menu finished
    But stupid zoom effect isn't properly working but im so mad idc anymore

--- main_menu.gd
+++ main_menu.gd
@@ -3,27 +3,30 @@ extends Node2D
 var button_type = null
 
 func _ready() -> void:
+	$"Fade Transition".show()
 	$"Fade Transition/AnimationPlayer".play("Fade_in")
+	await $"Fade Transition/AnimationPlayer".animation_finished
+	$"Fade Transition".hide()
 
 func _on_start_pressed() -> void:
 	button_type = "start"
 	$"Fade Transition".show()
 	$"Fade Transition/Fade_Timer".start()
-	$"Fade Transition/AnimationPlayer".play("Fade_in")
+	$"Fade Transition/AnimationPlayer".play("Fade_out")
 
 
 func _on_options_pressed() -> void:
 	button_type = "options"
 	$"Fade Transition".show()
 	$"Fade Transition/Fade_Timer".start()
-	$"Fade Transition/AnimationPlayer".play("Fade_in")
+	$"Fade Transition/AnimationPlayer".play("Fade_out")
 
 
 func _on_quit_pressed() -> void:
 	button_type = "quit"
 	$"Fade Transition".show()
 	$"Fade Transition/Fade_Timer".start()
-	$"Fade Transition/AnimationPlayer".play("Fade_in")
+	$"Fade Transition/AnimationPlayer".play("Fade_out")
 	get_tree().quit()
 
 
