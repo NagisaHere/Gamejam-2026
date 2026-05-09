7e7ab22835299717bc6f370bd6e71f16e64c5215
Augustus Chong <225411179+Augustus-Chong@users.noreply.github.com>
2026-05-09 17:05:43 +1000

    save before merge

---
+++ scoreboard.gd
@@ -0,0 +1,14 @@
+extends RichTextLabel
+
+var scores = [
+	{"name":"default1", "time left": "00:00", "fingers left": 0},
+	{"name":"default2", "time left": "00:00", "fingers left": 0},
+]
+
+# Called when the node enters the scene tree for the first time.
+func add_user(scores):
+	pass
+	
+func update_user_score(scores):
+	pass
+	
