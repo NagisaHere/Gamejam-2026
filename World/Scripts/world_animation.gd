255939ccfdf0c5794f28c493c5d6870564c4be03
Augustus Chong <225411179+Augustus-Chong@users.noreply.github.com>
2026-06-30 22:30:55 +0800

    added press escape to main menu button functionality

--- World/Scripts/world_animation.gd
+++ World/Scripts/world_animation.gd
@@ -33,3 +33,7 @@ func _on_button_2_pressed() -> void:
 	$tutorial.stop()
 	$CanvasLayer/Button.show()
 	$CanvasLayer/Button2.hide()
+	
+func _unhandled_input(event: InputEvent) -> void:
+	if Input.is_action_just_pressed("GoMainMenu"):
+		get_tree().change_scene_to_file("res://Gus additions/Menu/main_menu.tscn")
