a1f750f8b9ff690d63e979ac2c04a58f2b203065
Augustus Chong <225411179+Augustus-Chong@users.noreply.github.com>
2026-07-02 14:36:52 +0800

    changed esc to mainmenu to a dedicated pausemenu page

---
+++ Gus additions/Settings/pause_menu.gd
@@ -0,0 +1,43 @@
+extends Control
+
+func _ready() -> void:
+	$".".hide()
+	$AnimationPlayer.play("RESET")
+
+func resume():
+	get_tree().paused =false
+	$AnimationPlayer.play_backwards("blur")
+	$".".hide()
+	
+func pause():
+	get_tree().paused = true
+	$AnimationPlayer.play("blur")
+	$".".show()
+	
+func test_esc():
+	if Input.is_action_just_pressed("Pause") and !get_tree().paused:
+		pause()
+		
+	elif Input.is_action_just_pressed("Pause") and get_tree().paused:
+		resume()
+		
+func _process(delta: float) -> void:
+	test_esc()
+
+func _on_resume_pressed() -> void:
+	resume()
+	
+
+func _on_restart_pressed() -> void:
+	resume()
+	get_tree().reload_current_scene()
+	
+
+func _on_main_menu_pressed() -> void:
+		resume()
+		get_tree().change_scene_to_file("res://Gus additions/Menu/main_menu.tscn")
+
+
+func _on_tutorial_pressed() -> void:
+		resume()
+		get_tree().change_scene_to_file("res://Tutorial/Tutorial.tscn")
