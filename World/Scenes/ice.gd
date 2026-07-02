cab557e6ae5f6485572a5166139eb37b0b12a8dd
Augustus Chong <225411179+Augustus-Chong@users.noreply.github.com>
2026-07-02 16:52:23 +0800

    preliminary freeze computer effect

---
+++ World/Scenes/ice.gd
@@ -0,0 +1,16 @@
+extends TextureRect
+
+@export var freeze_duration = 10.0
+@export var max_ice_scale = Vector2(1.4, 1.4)
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	await get_tree().process_frame
+	pivot_offset = size/2.0
+	start_ice_freeze()
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func start_ice_freeze():
+	var tween = create_tween()
+	tween.tween_property(self, "scale", max_ice_scale, freeze_duration).set_trans(Tween.TRANS_LINEAR).from(Vector2(2.0, 2.0))
